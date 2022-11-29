f0f51ba09cfe2283b97477259829730a1f482129
tonylipch <91372267+tonylipch@users.noreply.github.com>
2022-11-29 14:23:09 +0100

    New User

---
+++ src/main/java/com/anton/stockexchanger/StockExchangerApplication.java
@@ -0,0 +1,13 @@
+package com.anton.stockexchanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockExchangerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockExchangerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anton/stockexchanger/StockExchangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anton.stockexchanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockExchangerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
